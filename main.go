5b7ece3d3469dd06d762d7884bd37a52bbed457b
Syaifuddin Hadi <syaifudinhadi8@gmail.com>
2023-04-14 14:08:25 +0800

    new update

---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+    "database/sql"
+    "fmt"
+    _ "github.com/go-sql-driver/mysql"
+)
+
+func connectToMySQL() (*sql.DB, error) {
+    // Replace the connection parameters with your own MySQL configuration
+    db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/vybesrawinfluencer")
+    if err != nil {
+        return nil, err
+    }
+    return db, nil
+}
+
+type InfluencerInterface struct {
+	URI string
+	Source string
+}
+
+func executeQuery(queryString string) (bool) {
+	db, err := connectToMySQL()
+	if err != nil {
+        return false
+    }
+    defer db.Close()
+    // Define the query to retrieve the URI and email columns from the influencerTable
+
+    // Execute the query and get the result set
+    fmt.Println(queryString)
+    __, err := db.Exec(queryString)
+    if err != nil {
+        return false
+    }
+    fmt.Println(__)
+
+    return true
+}
+
+func getDataFromQuery(queryString string) ([]InfluencerInterface, error) {
+	db, err := connectToMySQL()
+	if err != nil {
+        return nil, err
+    }
+    defer db.Close()
+    // Define the query to retrieve the URI and email columns from the influencerTable
+
+    // Execute the query and get the result set
+    rows, err := db.Query(queryString)
+    if err != nil {
+        return nil, err
+    }
+    defer rows.Close()
+
+    // Create a slice to hold the results
+    influencers := []InfluencerInterface{}
+
+    // Loop through the rows and create an InfluencerInterface struct for each row
+    for rows.Next() {
+        influencer := InfluencerInterface{}
+
+        // Scan the values from the row into the Influencer struct
+        err := rows.Scan(&influencer.URI, &influencer.Source)
+        if err != nil {
+            return nil, err
+        }
+
+        // Add the Influencer struct to the results slice
+        influencers = append(influencers, influencer)
+    }
+
+    // Check for errors that may have occurred during iteration over the rows
+    if err := rows.Err(); err != nil {
+        return nil, err
+    }
+
+    // Return the list of Influencer structs
+    return influencers, nil
+}
+
+func main() {
+	var queryString string
+	var tableName string
+	var uri string
+	var source string
+	var updateCommand string
+	queryString = "SELECT rawinfluencer.uri, rawinfluencer.source FROM rawinfluencer JOIN ( SELECT uri, selectedUserId FROM (SELECT uri, COUNT(id) AS userAmount, MIN(id) AS selectedUserId FROM rawinfluencer GROUP BY uri ) AS influencerGroup WHERE userAmount > 1) AS duplicatedInfluencers ON duplicatedInfluencers.uri = rawinfluencer.uri AND selectedUserId = rawinfluencer.id"
+
+	influencers, isSuccess := getDataFromQuery(queryString) 
+	for _, influencer := range influencers {
+		uri = influencer.URI
+		source = influencer.Source
+		tableName = "rawinfluencer"
+		updateCommand = "DELETE FROM " + tableName + "  WHERE source != '" + source + "' AND uri = '" + uri + "'"
+		executeQuery(updateCommand)
+	}
+	fmt.Println(isSuccess)
+}
